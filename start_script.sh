7a1a799844c8d492aba87b009fd388f4f4f9e7c3
AnkitaMungalpara <mungalpara.ankita@gmail.com>
2024-12-10 14:48:43 -0500

    workflow update docker install

---
+++ start_script.sh
@@ -0,0 +1,15 @@
+sudo apt update
+apt install curl -y
+sudo apt install curl -y
+sudo apt install unzip -y
+sudo apt install vim -y
+sudo curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
+sudo unzip awscliv2.zip
+sudo ./aws/install --update
+/usr/local/bin/aws --version
+aws s3 ls
+sudo apt install npm
+node --version
+npm --version
+sudo apt-get update
+sudo apt-get install -y docker.io
